a6a3ae3092c2f4ecb68b71101ad2d3450af4fe3a
MossoLuciano <luchomosso30@gmail.com>
2021-02-16 09:40:24 -0300

    Primer iteracion: maquetado

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <h3>Nuevo Banco Max</h3>
+    <p>Telefono: 03404-514785 / 03404-578912</p>
+    <p>Dirección: Mitre 123</p>
+    <p>Email: bancomax@gmail.com</p>
+</div>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require "header.php";
+?>
+
+    <div class="titulo"><h1>Bienvenidos a nuestro sistema</h1></div>
+
+<?php
+    require "footer.php";
+?>
