ee71874932030941ed70fa00b48702441625d6d4
MossoLuciano <luchomosso30@gmail.com>
2021-02-16 09:54:44 -0300

    Creacion de mas archivos y de navBar

--- header.php
+++ header.php
@@ -5,10 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="master.css">
 </head>
 <body>
     <div class="header">
-        <div class="logo"> <img src="img/macro-logo.png" alt="logo"></div>
+        <div class="logo"> 
+            <a href="index.php"><img src="img/macro-logo.png" alt="logo"></a>
+        </div>
+        <div class="navBar">
+                <ul class="opciones">
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="contacto.php">Contacto</a>
+                    </li>
+                </ul>
+            </div>
     </div>
 </body>
 </html>
