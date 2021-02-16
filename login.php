a1f9f6baa9fb781dfb2415adbc46378cec83a19f
MossoLuciano <luchomosso30@gmail.com>
2021-02-16 10:35:53 -0300

    Armado del archivo login

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+    require "header.php";
+?>
+
+<div class="login">
+    <h1 class="tituloLogin">Login</h1>
+    <form action="#" method="POST">
+        <input type="email" name="email" class="email" placeholder="Email"><br><br>
+        <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
+        <input type="submit" value="Ingresar" name="ingresar" class="botonIngresar">
+    </form>
+</div>
+
+<?php
+    require "footer.php";
+?>
