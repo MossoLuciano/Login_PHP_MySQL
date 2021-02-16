97f6aaaa5691bd6882524344b15fdd39cba04b90
MossoLuciano <luchomosso30@gmail.com>
2021-02-16 11:07:36 -0300

    Aplicacion de estilos

--- login.php
+++ login.php
@@ -3,11 +3,12 @@
 ?>
 
 <div class="login">
-    <h1 class="tituloLogin">Login</h1>
-    <form action="#" method="POST">
+    <h1 class="tituloLogin">Login</h1><br>
+    <form action="#" method="POST" class="formularioLogin">
         <input type="email" name="email" class="email" placeholder="Email"><br><br>
         <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
-        <input type="submit" value="Ingresar" name="ingresar" class="botonIngresar">
+        <input type="submit" value="Ingresar" name="ingresar" class="botonIngresar"><br><br>
+        <a href="registro.php">Olvide mi clave</a>
     </form>
 </div>
 
